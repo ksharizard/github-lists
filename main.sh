68bf6cad9632a87146a4aa3b9ff5314f24b2e780
ksharizard <code@kshdev.slmail.me>
2026-05-16 15:19:03 +0530

    feat: add warn func and turn the output funcs to one liners

--- main.sh
+++ main.sh
@@ -6,17 +6,10 @@ COOKIE_FILE="cookie.txt"
 
 trap 'rm "$all_stars_file" "$in_lists_file"' EXIT
 
-error() {
-    echo -e "\033[31m$1\033[0m"
-}
-
-success() {
-    echo -e "\033[32m$1\033[0m"
-}
-
-info() {
-    echo -e "\033[34m$1\033[0m"
-}
+error() { echo -e "\033[31m$1\033[0m"; }
+info() { echo -e "\033[34m$1\033[0m"; }
+success() { echo -e "\033[32m$1\033[0m"; }
+warn() { echo -e "\033[33m$1\033[0m"; }
 
 select_multiple_lists() {
     local selected=()
